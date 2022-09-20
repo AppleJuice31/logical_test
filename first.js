113f672af092f8711b114a8876cfb780c8719dfd
AppleJuice31 <112159249+AppleJuice31@users.noreply.github.com>
2022-09-20 12:59:10 +0530

    Update first.js

--- first.js
+++ first.js
@@ -12,6 +12,7 @@ function printAlphabets(alphabetCase) {
     else {
         alert("enter either small or capital")
     }
+    console.log(start);
     // loop through the values from i to j
     // for (k = start; k < end; k++) {
     //     //convert the char code to string (Alphabets)
@@ -20,9 +21,12 @@ function printAlphabets(alphabetCase) {
     //     console.log(str);
     // }
 
+    var n = 3
+    var k = start;
+
     for (j = 0; j < n; j++) {
+        // var temp = []
         for (let i = n - 1; i >= 0; i--) {
-            var k = start;
             // if (k == end) {
             //     console.log('end');
             // }
@@ -36,53 +40,7 @@ function printAlphabets(alphabetCase) {
 }
 
 
-// var start = 65;
-// var end = 91;
-
-
-// function printAlphabets(n) {
-//     for (j = 0; j < n; j++) {
-
-//         for (let i = n - 1; i >= 0; i--) {
-//             console.log('s');
-//         }
-//         console.log("\n");
-//     }
-// }
-
-
-// printAlphabets(3);
 
 var alphabetCase = prompt("Enter small or capital", "capital")
 console.log(alphabetCase);
 printAlphabets(alphabetCase);
-
-
-// function print(a, m) {
-//     var b = Array(m * m);
-//     for (i = 0; i < m * m; i++)
-//         for (j = 0; j < m; j++)
-//             b[j] = b[i * m + j] = a[j];
-//     return b;
-// }
-
-// const alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// var m = 26;
-// let c = print(alpha, m)
-// function alphaMatrix(n) {
-//     var matrixSize = n * n;
-//     var matrixElements = [];
-//     var matrixRows = [];
-
-//     for (i = 0; i < matrixSize; i++) {
-//         matrixElements.push(c[i]);
-//     }
-//     for (j = 0; j < n; j++) {
-//         matrixRows.push(matrixElements.splice(0, n));
-//     }
-//     output = matrixRows[0].map((_, colIndex) => matrixRows.map(row => row[colIndex]));
-//     return (output.reverse())
-
-// }
-// let res = alphaMatrix(3)
-// console.log(res)
